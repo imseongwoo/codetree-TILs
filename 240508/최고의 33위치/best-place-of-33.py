4362162a4f79e176758eebfd84ee73c431ac6828
임성우 <seongwoorang@naver.com>
2024-05-08 17:20:57 +0900

    Title: 최고의 33위치, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240508/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = []
+answer = -1
+for _ in range(n):
+    arr.append(list(map(int,input().split())))
+
+for i in range(len(arr)):
+    for j in range(len(arr[i])):
+        if i+2 >= n:
+            continue
+        if j+2 >= n:
+            continue
+        temp = 0
+
+        for a in range(i,i+3):
+            for b in range(j,j+3): 
+                temp += arr[a][b]
+        
+    if temp > answer:
+        answer = temp
+print(answer)
