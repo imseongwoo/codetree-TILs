8ddb8d941467c201aa53096f9850573d7fd8070e
임성우 <seongwoorang@naver.com>
2024-08-29 16:11:27 +0900

    Title: 1차원 폭발 게임, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240829/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240829/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,25 +1,41 @@
-n, m = map(int, input().split())
+def bomb_simulation(N, M, bombs):
+    while True:
+        found_explosion = False
+        i = 0
+        
+        # List to collect the new state of the bombs after explosions
+        new_bombs = []
+        
+        while i < len(bombs):
+            count = 1
+            
+            # Check how many consecutive bombs have the same number
+            while i + count < len(bombs) and bombs[i + count] == bombs[i]:
+                count += 1
+            
+            # If the sequence length is M or more, we skip adding these to new_bombs (they explode)
+            if count >= M:
+                found_explosion = True
+                i += count  # Skip this sequence as it explodes
+            else:
+                new_bombs.extend(bombs[i:i + count])
+                i += count
+        
+        # Update the bombs list with the new state
+        bombs = new_bombs
+        
+        # If no explosion occurred, we're done
+        if not found_explosion:
+            break
+    
+    # Print the result
+    print(len(bombs))
+    for bomb in bombs:
+        print(bomb)
 
-arr = [int(input()) for _ in range(n)]
+# Input reading
+N, M = map(int, input().split())
+bombs = [int(input()) for _ in range(N)]
 
-def check_bombs():
-    global arr
-    i = 0
-    while i < len(arr):
-        num = arr[i]
-        cnt = 1
-        while i + cnt < len(arr) and arr[i + cnt] == num:
-            cnt += 1
-
-        if cnt >= m:
-            del arr[i:i + cnt] 
-            return True  
-        i += cnt
-    return False  
-
-while check_bombs():
-    pass
-
-print(len(arr))
-for num in arr:
-    print(num)
+# Call the function with the input data
+bomb_simulation(N, M, bombs)
