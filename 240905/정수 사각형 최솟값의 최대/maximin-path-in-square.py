4dd4024a4ead766d70b63c74b139ddc070e7b02d
임성우 <seongwoorang@naver.com>
2024-09-05 14:24:45 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 158ms, Memory: 25MB, Status: Passed - CodeTree

--- 240905/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 240905/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -1,33 +1,17 @@
-def max_of_min_path(matrix, N):
-    # DP 테이블 초기화
-    dp = [[0] * N for _ in range(N)]
-    
-    # 첫 번째 셀 초기화
-    dp[0][0] = matrix[0][0]
-    
-    # 첫 번째 행 채우기 (오직 왼쪽에서 올 수 있음)
-    for j in range(1, N):
-        dp[0][j] = min(dp[0][j-1], matrix[0][j])
-    
-    # 첫 번째 열 채우기 (오직 위에서 올 수 있음)
-    for i in range(1, N):
-        dp[i][0] = min(dp[i-1][0], matrix[i][0])
-    
-    # 나머지 DP 테이블 채우기
-    for i in range(1, N):
-        for j in range(1, N):
-            dp[i][j] = max(
-                min(dp[i-1][j], matrix[i][j]),  # 위에서 오는 경우
-                min(dp[i][j-1], matrix[i][j])   # 왼쪽에서 오는 경우
-            )
-    
-    # 결과는 우하단에 저장됨
-    return dp[N-1][N-1]
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
 
-# 입력 처리
-N = int(input())
-matrix = [list(map(int, input().split())) for _ in range(N)]
+dp[0][0] = arr[0][0]
 
-# 결과 계산 및 출력
-result = max_of_min_path(matrix, N)
-print(result)
+for i in range(1, n):
+    dp[i][0] = min(dp[i - 1][0], arr[i][0])
+
+for j in range(1, n):
+    dp[0][j] = min(dp[0][j - 1], arr[0][j])
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = min(max(dp[i - 1][j], dp[i][j - 1]), arr[i][j])
+
+print(dp[n - 1][n - 1])
