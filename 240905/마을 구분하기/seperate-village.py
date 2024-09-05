74a01efe9ac2cb5af940d8db0c8380241d97ecaf
임성우 <seongwoorang@naver.com>
2024-09-05 16:25:19 +0900

    Title: 마을 구분하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240905/마을 구분하기/seperate-village.py
@@ -0,0 +1,37 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * n for _ in range(n)]
+town_member = []
+member = 1
+
+def dfs(x, y):
+    global town_member
+    global member
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] != 0 and visited[nx][ny] != 1:
+            member += 1
+            visited[nx][ny] = 1
+            dfs(nx,ny)
+
+    if member != 1:
+        town_member.append(member)
+        member = 1
+
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1 and visited[i][j] != 1:
+            visited[i][j] = 1
+            dfs(i,j)
+
+print(len(town_member))
+town_member.sort()
+for a in town_member:
+    print(a)
