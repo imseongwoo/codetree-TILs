6c6fa96b56d12740b66004adb6a15788cd241b65
임성우 <seongwoorang@naver.com>
2024-09-28 20:04:52 +0900

    Title: 떨어지는 1자 블록, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240928/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240928/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,23 +1,24 @@
 n, m, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-k -= 1
-final_row = -1
+k -= 1  # 시작 열을 0 기반 인덱스로 조정
+final_row = n - 1  # 블록이 맨 아래에 떨어진다고 가정
 
+# 블록이 멈출 행을 찾는 로직
 for row in range(n - 1):
+    can_fall = True
     for i in range(k, k + m):
-        if arr[row][i] == 1:
-            final_row = row - 1
+        if arr[row + 1][i] == 1:  # 블록이 장애물에 부딪히는지 확인
+            final_row = row
+            can_fall = False
             break
-    else:
-        final_row = max(final_row, row)
-else:
-    final_row = n - 1
+    if not can_fall:
+        break
 
+# 블록을 그리드에 배치
 for i in range(k, k + m):
     arr[final_row][i] = 1
 
+# 최종 그리드 상태 출력
 for i in range(n):
-    for j in range(n):
-        print(arr[i][j], end=' ')
-    print()
+    print(' '.join(map(str, arr[i])))
