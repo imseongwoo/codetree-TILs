5f1e94cec47e6b6a9284c4de1ecd506be0b63c7a
임성우 <seongwoorang@naver.com>
2024-09-28 19:53:14 +0900

    Title: 떨어지는 1자 블록, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240928/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,21 @@
+n, m, k = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+k -= 1
+final_row = -1
+
+for row in range(n - 1):
+    for i in range(k, k + m):
+        if arr[row][i] == 1:
+            final_row = row - 1
+            break
+    else:
+        final_row = max(final_row, row)
+
+for i in range(k, k + m):
+    arr[final_row][i] = 1
+
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end=' ')
+    print()
