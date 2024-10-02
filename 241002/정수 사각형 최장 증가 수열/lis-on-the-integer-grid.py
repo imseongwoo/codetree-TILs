07a51a98508ddfe923c3d11a53fad7fd690fec9e
임성우 <seongwoorang@naver.com>
2024-10-02 21:32:28 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 362ms, Memory: 63MB, Status: Passed - CodeTree

--- 241002/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 241002/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,20 +1,34 @@
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-dp = [[1]*n for _ in range(n)]
+import sys
+sys.setrecursionlimit(100000)
 
-dx = [-1, 1 , 0, 0]
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-for x in range(n):
-    for y in range(n):
-        for a in range(4):
-            nx = x + dx[a]
-            ny = y + dy[a]
+def dfs(x, y, arr, dp, n):
+    if dp[x][y] != -1:
+        return dp[x][y]
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if arr[nx][ny] < arr[x][y]:
-                    dp[x][y] = max(dp[x][y], dp[nx][ny] + 1)
-max_val = []
-for i in range(n):
-    max_val.append(max(dp[i]))
-print(max(max_val))
+    dp[x][y] = 1
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] > arr[x][y]:
+            dp[x][y] = max(dp[x][y], dfs(nx, ny, arr, dp, n) + 1)
+    
+    return dp[x][y]
+
+def find_max_steps(n, arr):
+    dp = [[-1] * n for _ in range(n)]
+    
+    max_steps = 0
+    for i in range(n):
+        for j in range(n):
+            max_steps = max(max_steps, dfs(i, j, arr, dp, n))
+    
+    return max_steps
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+print(find_max_steps(n, arr))
