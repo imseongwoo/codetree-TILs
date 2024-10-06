e8350db077bccc9183f3ee8cffb87973c8d5f97e
임성우 <seongwoorang@naver.com>
2024-10-06 17:27:36 +0900

    Title: 마법의 숲 탐색, Time: 495ms, Memory: 29MB, Status: Passed - CodeTree

--- 241006/마법의 숲 탐색/magical-forest-exploration.py
+++ 241006/마법의 숲 탐색/magical-forest-exploration.py
@@ -1,3 +1,9 @@
+# 1. 남쪽으로 한 칸 내려가기
+# 2. 남쪽으로 못가면 서쪽으로 회전한후 한칸 내려가기
+# 3. 서쪽으로 회전 못하면 동쪽으로 회전하고 한칸 내려가기
+# 4. 갈 수 있는 가장 남쪽의 칸으로 이동, 만약 골렘의 몸 일부가 숲을 벗어난 상태면 맵을 초기화하고 다음 값 탐색
+# 5. 출구가 다른 골렘과 인접하고 있다면 다른 골렘으로 이동 가능
+
 from collections import deque
 
 r, c, k = map(int, input().split())
@@ -7,14 +13,17 @@ dy = [0, 1, 0, -1]
 isExit = [[False] * c for _ in range(r + 3)]
 answer = 0
 
+
 def canGo(x, y):
     return (0 <= y - 1 and y + 1 < c and 0 <= x - 1 and x + 1 < r + 3 and
             arr[x - 1][y - 1] == 0 and arr[x - 1][y] == 0 and arr[x - 1][y + 1] == 0 and
             arr[x][y - 1] == 0 and arr[x][y] == 0 and arr[x][y + 1] == 0 and
             arr[x + 1][y] == 0)
 
+
 def inRange(x, y):
     return 3 <= x < r + 3 and 0 <= y < c
+
 
 def down(x, y, d, id):
     if canGo(x + 1, y):
@@ -37,11 +46,13 @@ def down(x, y, d, id):
             global answer
             answer += bfs(x, y) - 3 + 1
 
+
 def resetMap():
     for i in range(r + 3):
         for j in range(c):
             arr[i][j] = 0
             isExit[i][j] = False
+
 
 def bfs(x, y):
     queue = deque()
@@ -56,15 +67,14 @@ def bfs(x, y):
             nx = cur_x + dx[i]
             ny = cur_y + dy[i]
             if inRange(nx, ny) and not visited[nx][ny] and (
-                arr[nx][ny] == arr[cur_x][cur_y] or (isExit[cur_x][cur_y] and arr[nx][ny] != 0)):
+                    arr[nx][ny] == arr[cur_x][cur_y] or (isExit[cur_x][cur_y] and arr[nx][ny] != 0)):
                 queue.append((nx, ny))
                 visited[nx][ny] = True
-                result = max(result, nx)  # Fix the map() error by using max()
-
+                result = max(result, nx)
     return result
+
 
 for id in range(1, k + 1):
     y, d = map(int, input().split())
     down(0, y - 1, d, id)
-
 print(answer)
