922b9155e3b8605c874d5dae5a64ab0502ce03ed
임성우 <seongwoorang@naver.com>
2024-10-18 16:23:45 +0900

    Title: 숫자의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241018/숫자의 개수/number-of-integers.py
@@ -0,0 +1,36 @@
+n,m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+def lower_bound(target):
+    left = 0
+    right = n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+    return min_idx
+
+
+def upper_bound(target):
+    left = 0
+    right = n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return min_idx
+
+for _ in range(n):
+    num = int(input())
+    print(upper_bound(num) - lower_bound(num))
