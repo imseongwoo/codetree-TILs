7b6a8171c79ac02ab46be2cff1b9bff2c6ebd4f0
임성우 <seongwoorang@naver.com>
2024-10-22 15:14:29 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241022/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n = int(input())
+visited = [0] * 1000002
+
+q = deque()
+q.append((n,0))
+visited[n] = 1
+operator = [1, -1, 2, 3]
+cnt = 0
+
+while q:
+    num, level = q.popleft()
+
+    if num == 1:
+        print(level)
+        break
+
+    for i in range(4):
+        new_num = 0
+        if i==2 or i==3:
+            if (num % operator[i]) == 0:
+                new_num = num // operator[i]
+            else:
+                continue
+        else:
+            new_num = num + operator[i]
+        if visited[new_num] == 0:
+            q.append((new_num, level + 1))
+            visited[new_num] = 1
+    cnt += 1
