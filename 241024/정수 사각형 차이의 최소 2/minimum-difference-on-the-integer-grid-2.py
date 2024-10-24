6d87fe47be821d6b14904686de64765e1e27a5ee
임성우 <seongwoorang@naver.com>
2024-10-24 16:16:28 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 3735ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 241024/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 241024/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -2,36 +2,34 @@ from copy import deepcopy
 
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-
 dp = [[[] for _ in range(n)] for _ in range(n)]
-dp[0][0].append(arr[0][0])  
+dp[0][0].append([arr[0][0]])
 
 for b in range(1, n):
-    temp = deepcopy(dp[0][b-1])  
-    temp.append(arr[0][b])       
-    dp[0][b] = temp             
+    temp = deepcopy(dp[0][b - 1])
+    for path in temp:
+        path.append(arr[0][b])
+    dp[0][b] = temp
 
 for a in range(1, n):
-    temp_top = deepcopy(dp[a-1][0])  
-    temp_top.append(arr[a][0])       
-    dp[a][0] = temp_top              
+    temp_top = deepcopy(dp[a - 1][0])
+    for path in temp_top:
+        path.append(arr[a][0])
+    dp[a][0] = temp_top
 
 for a in range(1, n):
     for b in range(1, n):
-        temp_from_top = deepcopy(dp[a-1][b])    
-        temp_from_left = deepcopy(dp[a][b-1])  
-        
-        temp_from_top.append(arr[a][b])
-        temp_from_left.append(arr[a][b])
-        
-        if max(temp_from_top) - min(temp_from_top) < max(temp_from_left) - min(temp_from_left):
-            dp[a][b] = temp_from_top
-        else:
-            dp[a][b] = temp_from_left
+        temp_from_top = deepcopy(dp[a - 1][b])
+        for path in temp_from_top:
+            path.append(arr[a][b])
 
-final_path = dp[n-1][n-1]
+        temp_from_left = deepcopy(dp[a][b - 1])
+        for path in temp_from_left:
+            path.append(arr[a][b])
 
-min_val = min(final_path)
-max_val = max(final_path)
+        dp[a][b] = temp_from_top + temp_from_left
+ans = []
+for path in dp[n - 1][n - 1]:
+    ans.append(abs(max(path)-min(path)))
 
-print(max_val - min_val)
+print(min(ans))
